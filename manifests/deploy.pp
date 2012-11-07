28c6348a960b0c9ebea4fdb1ef6bc8861013cddb
Davi Campos <arlocm@gmail.com>
2012-11-07 16:47:28 -0200

    Refactoring and support new conecpt of virtualenv class

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -1,11 +1,15 @@
 define django::deploy(
   $app_name = $name,
-  $env_path,
+  $venv_name = $name,
+  $project_path,
+  $clone_path = "src",
   $git_url,
   $user,
   $settings = "settings_local.py",
   $settings_source = undef,
+  $requirements = "requirements.txt",
   $migrate = false,
+  $collectstatic = false,
   $bind = "0.0.0.0:8000",
   $backlog = undef,
   $workers = "multiprocessing.cpu_count() * 2 + 1",
@@ -20,98 +24,98 @@ define django::deploy(
 
 ) {
 
-  # Create virtualenv
-  virtualenv::create { $env_path:
-    user => $user,
-    require => Class['virtualenv']
+  if $clone_path == "" {
+    $source_path = $project_path
+  } else {
+    $source_path = "${project_path}/${clone_path}"
   }
 
-  # Create directory to source code
-  file { "${env_path}/src/":
-      ensure => directory,
-      owner => $user,
-      require => Virtualenv::Create[$env_path]
-  }
-
-  # Clone source
+  # Clone APP
   exec { "git-clone ${app_name}":
-    command => "git clone ${git_url} ${app_name}",
-    cwd => "${env_path}/src/",
-    require => [Package['git'], File["${env_path}/src/"]],
-    user => $user,
-    path => ['/usr/bin']
+    command => "git clone ${git_url} ${source_path}",
+    user    => $user,
+    path    => ['/usr/bin'],
+    unless  => "test -d ${source_path}",
+    require => Package['git'],
+  }
+
+  # Create virtualenv
+  virtualenv::create { $venv_name:
+    user    => $user,
+    project => $project_path,
+    require => Class['virtualenv'],
   }
 
   # Create settings file
   if $settings_source {
     file { "settings ${app_name}":
-      ensure => present,
-      path => "${env_path}/src/${app_name}/${settings}",
-      source => $settings_source,
-      owner => $user,
+      ensure  => present,
+      path    => "${source_path}/${settings}",
+      source  => $settings_source,
+      owner   => $user,
       require => Exec["git-clone ${app_name}"]
     }
   } else {
     file { "settings ${app_name}":
-      ensure => present,
-      path => "${env_path}/src/${app_name}/${settings}",
-      owner => $user,
+      ensure  => present,
+      path    => "${source_path}/${settings}",
+      owner   => $user,
       require => Exec["git-clone ${app_name}"],
     }
   }
 
   # Install requirements
-  virtualenv::install_requirements { "${env_path}/src/${app_name}/requirements.txt":
-    virtualenv => $env_path,
-    require => File["settings ${app_name}"]
+  virtualenv::exec { "requirements ${app_name}":
+    virtualenv => $venv_name,
+    user       => $user,
+    command    => "pip install -r ${source_path}/${requirements}",
+    require    => File["settings ${app_name}"],
   }
 
   # Run syncdb
-  exec { "syncdb ${app_name}":
-    command => 'python manage.py syncdb --noinput',
-    path => "${env_path}/bin/",
-    cwd => "${env_path}/src/${app_name}",
-    user => $user,
-    require => Virtualenv::Install_requirements["${env_path}/src/${app_name}/requirements.txt"]
+  virtualenv::exec { "syncdb ${app_name}":
+    virtualenv => $venv_name,
+    command    => 'python ${source_path}/manage.py syncdb --noinput',
+    user       => $user,
+    require    => Virtualenv::Exec["requirements ${app_name}"],
   }
 
   # Run collectstatic
-  exec { "collectstatic ${app_name}":
-    command => 'python manage.py collectstatic --noinput',
-    path => "${env_path}/bin/",
-    cwd => "${env_path}/src/${app_name}",
-    user => $user,
-    require => Exec["syncdb ${app_name}"]
+  if ($collectstatic) {
+    virtualenv::exec { "collectstatic ${app_name}":
+      virtualenv => $venv_name,
+      command    => 'python ${source_path}/manage.py collectstatic --noinput',
+      user       => $user,
+      require    => Virtualenv::Exec["syncdb ${app_name}"],
+      before     => Supervisor::App[$app_name],
+    }
   }
-
   # Run migrate
   if ($migrate) {
-    exec { "collectstatic ${app_name}": 
-      command => 'python manage.py migrate --noinput',
-      path => "${env_path}/bin/",
-      cwd => "${env_path}/src/${app_name}",
-      user => $user,
-      require => Exec["syncdb ${app_name}"]
+    virtualenv::exec { "migrate ${app_name}": 
+      virtualenv => $venv_name,
+      command    => 'python ${source_path}/manage.py migrate --noinput',
+      user       => $user,
+      require    => Virtualenv::Exec["syncdb ${app_name}"],
+      before     => Supervisor::App[$app_name],
     }
   }
 
   # Create gunicorn conf file
-  file { "${env_path}/src/gunicorn.conf.py":
-    ensure => present,
+  file { "gunicorn ${app_name}":
+    path    => "${project_path}/gunicorn.conf.py",
+    ensure  => present,
     content => template("django/gunicorn.conf.py.erb"),
-    owner => $user,
-    require => Virtualenv::Create[$env_path]
+    owner   => $user,
+    require => Virtualenv::Create[$venv_name]
   }
 
   # Configure supervisor to run django
   supervisor::app { $app_name:
-    command => "${env_path}/bin/gunicorn_django -c ../gunicorn.conf.py",
-    directory => "${env_path}/src/${app_name}",
+    command => "\$HOME/.virtualenvs/${venv_name}/bin/gunicorn_django -c ../gunicorn.conf.py",
+    directory => "${source_path}",
     user => $user,
-    require => [
-      Exec["collectstatic ${app_name}"],
-      File["${env_path}/src/gunicorn.conf.py"]
-    ]
+    require => File["gunicorn ${app_name}"],
   }
 
 
